24d6007e516d46dfa1fe55102032374b306ebc77
Pedro <mafuzao@outlook.com>
2021-08-29 20:21:30 -0300

    Criando Componentes dos botões Atualizar, enviar e Deletar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
-import Formulario from './Components/Formulario/formulario';
+import Formulario from './Components/Formulario/Formulario';
+import EnviarButton from './Components/Button/Enviar/Enviar';
+import DeletarButton from './Components/Button/Deletar/Deletar';
+import AtualizarButton from './Components/Button/Atualizar/Atualizar';
 
 function App() {
   return (
     <div className="App">
       <Formulario>
-
+          
       </Formulario>
+      <EnviarButton></EnviarButton>
       
     </div>
   );
---
+++ src/Components/Button/Enviar/Enviar.js
@@ -0,0 +1,16 @@
+import { Component } from "react";
+
+
+
+
+class EnviarButton extends Component{
+    render(){  
+        return(
+            <div>
+                <button>Enviar</button>
+            </div>
+        ) 
+    }
+}
+
+export default EnviarButton
